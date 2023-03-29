2845a353e263c9e73eddefb948d4856fc0771805
DaniilShilov <shilovdanv@gmail.com>
2023-03-29 18:31:33 +0400

    add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Enter a number: ");
+
+int a = int.Parse(Console.ReadLine());
+
+  if(a % 2 == 0)
+  {
+    System.Console.WriteLine("Even");
+  }
+  else
+  {
+    System.Console.WriteLine("Odd");
+  }
