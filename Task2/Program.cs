d9ae60c845d7a0a68bc4572cb7795cdb2a13ac56
DaniilShilov <shilovdanv@gmail.com>
2023-03-29 17:06:15 +0400

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿int a;
+int b;
+
+System.Console.WriteLine("Enter a number: ");
+
+if(int.TryParse(Console.ReadLine(), out a) && int.TryParse(Console.ReadLine(), out b))
+{
+  if(a > b)
+  {
+    System.Console.WriteLine("max = " + a);
+    System.Console.WriteLine("min = " + b);
+  }
+  else
+  {
+    System.Console.WriteLine("max = " + b);
+    System.Console.WriteLine("min = " + a);
+  }
+}
+else
+{
+  System.Console.WriteLine("Error!");
+}
