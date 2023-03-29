6622e95e24efdcda31b757e7c1401523a46e1fbe
DaniilShilov <shilovdanv@gmail.com>
2023-03-29 17:26:31 +0400

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿int a;
+int b;
+int c;
+
+System.Console.WriteLine("Enter a three numbers: ");
+
+if(int.TryParse(Console.ReadLine(), out a) &&
+   int.TryParse(Console.ReadLine(), out b) &&
+   int.TryParse(Console.ReadLine(), out c))
+{
+  if(a > b &&  a > c)
+  {
+    System.Console.WriteLine("max = " + a);
+  }
+  else if(b > a && b > c)
+  {
+    System.Console.WriteLine("max = " + b);
+  }
+  else if(c > a && c > b)
+  {
+    System.Console.WriteLine("max = " + c);
+  }
+}
+else
+{
+  System.Console.WriteLine("Error!");
+}  
